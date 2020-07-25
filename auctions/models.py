2d22b7300811697bbaa20eed37ec999a3034a249
Graeme-Code <graemebarnes1987@gmail.com>
2020-07-25 15:03:47 +0200

    create listing, view listing and update bid, still require validations and error handling

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,43 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import URLValidator
+from django.utils import timezone
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    category = models.CharField(max_length=64, null=True) #use a data list on form to provide options of what categories already exist. 
+    imageURL = models.URLField(null=True, validators =[URLValidator])
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    #create key to get user ID. 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    starting_bid = models.PositiveIntegerField()
+
+
+    def __str__(self):
+        return f"ID: {self.id} Title:{self.title} Description:{self.description} Category:{self.category} ImageURL:{self.imageURL} Active:{self.active} Created at:{self.created_at} UserID:{self.user})"
+   
+class Comments(models.Model):
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    #listing = 
+    #user = 
+    pass
+
+class Bids(models.Model):
+    #starting bid ID #this should be in the listing model
+    created_at = models.DateTimeField(auto_now_add=True)
+    bid = models.PositiveIntegerField()
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Bid: {self.bid} listing ID:{self.listing.id} User_ID:{self.user})"
+       
+
+    
--- auctions/views.py
+++ auctions/views.py
@@ -3,12 +3,51 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User
+from .models import User, Listing, Bids
 
+class CreateListing(forms.Form): #note to self classes are in camelcase! 
+    title = forms.CharField(label='Listing Title', max_length=64, min_length=4)
+    description = forms.CharField(widget=forms.Textarea)
+    category = forms.CharField(label='Category', max_length=64)
+    imageURL = forms.URLField()
+    starting_bid = forms.IntegerField(label="Starting Bid")
+
+class NewBid(forms.Form):
+    newbid = forms.IntegerField(label="New Bid") #This will need validation to ensure new bid is greater than old bid
 
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html", {
+        "active_listings":Listing.objects.all()
+    })
+
+def listing(request, listing_id):
+    if request.method == "POST":
+        listing = Listing.objects.get(pk=listing_id)
+        user = User.objects.get(pk = int(request.user.id))
+        user_id = user.id
+        newbid_value = request.POST['newbid']
+        newbid = Bids(bid=newbid_value, listing_id=listing_id, user_id=user_id)
+        newbid.save()
+        return HttpResponseRedirect(reverse("listing",args=([listing_id]),
+        ))
+    else:
+        #get listing
+        listing = Listing.objects.get(pk=listing_id)
+        #get bids
+        bids = Bids.objects.filter(listing_id=listing_id).order_by("-bid")
+        #get number of bids
+        bids_count = bids.count()
+        #get the highest bid
+        highest_bid = bids.first().bid
+        return render(request, "auctions/listing.html", {
+            "listing":listing,
+            "bid_count": bids_count,
+            "highest_bid": highest_bid,
+            "newbid": NewBid
+        })
+
 
 
 def login_view(request):
@@ -61,3 +100,25 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def createlisting(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        description = request.POST['description']
+        category = request.POST['category']
+        imageURL = request.POST['imageURL']
+        starting_bid = request.POST['starting_bid']
+        user = request.user.id #this gives me the user name. 
+        user_id = User.objects.get(pk = int(user)) #trying to get rest of data of user
+        print(user)
+        print(user_id)
+        newlisting = Listing(title=title, description=description, category=category, imageURL=imageURL, starting_bid=starting_bid)
+        newlisting.user = user_id
+        newlisting.save()
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/createlisting.html", {
+            'form':CreateListing()
+            })
+
+    
